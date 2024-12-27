35c5cff70aac20da7ce0dc8b6abeff7c0d364d0f
aygerim <aygerim.kasymova.94@internet.ru>
2024-12-27 19:09:31 +0600

    arhiv

---
+++ src/main/fruit/apple.java
@@ -0,0 +1,4 @@
+package main.fruit;
+
+public class apple {
+}
---
+++ src/test/java/bugfix/Bugfix1.java
@@ -0,0 +1,9 @@
+package bugfix;
+
+public class Bugfix1 {
+    public static void main(String[] args) {
+        System.out.println("Bug Fix 2");
+    }
+
+
+}
